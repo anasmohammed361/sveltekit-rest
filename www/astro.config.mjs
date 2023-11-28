8320c6a7a6473519c9866ee065d3f9af91f98db3
anasmohammed361 <anasmohammed361@gmail.com>
2023-11-29 01:20:24 +0530

    made chagnes to site

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -1,27 +1,31 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [starlight({
+    title: 'Sveltekit-Rest',
+    customCss:['./src/tailwind.css'],
+    social: {
+      github: 'https://github.com/anasmohammed361/sveltekit-rest',
+    },
+    sidebar: [{
+      label: 'Getting Started',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Quick Start',
+        link: '/guides/example/'
+      }]
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  }), tailwind({
+    applyBaseStyles:false
+  })]
 });
