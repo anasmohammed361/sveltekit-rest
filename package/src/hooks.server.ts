173a41f87334e25d1b8f33b37b6fc14b7787701c
Coder-Vasen <vasen2353@gmail.com>
2023-12-06 17:07:06 +0530

    Removed my testing fiels

--- package/src/hooks.server.ts
+++
@@ -1,4 +0,0 @@
-import type { Handle } from '@sveltejs/kit';
-import { RestHandle } from './routes/chumma.js';
-
-export const handle: Handle = RestHandle;
--- package/src/routes/+page.svelte
+++ package/src/routes/+page.svelte
@@ -1,35 +1,3 @@
-<script lang="ts">
-	import type { PageServerData } from './$types.js';
-	import { client } from './chumma.js';
 
-	let arr: any = [];
-	export let data: PageServerData;
-	console.log(data);
-	const users = client.users({ name: 'Kirthevasen' });
-</script>
-
-{#each arr as a}
-	<li>{a?.name}</li>
-{/each}
-
-{#if $users.isLoading}
-	Loading
-{:else if $users.isSetteled}
-	Data {$users.data?.name}
-{:else if $users.isError}
-	Error {$users.error?.message}
-{/if}
-
-<button
-	on:click={async () => {
-		console.log(await $users.refetch());
-	}}>Refetch users</button
->
 
 <p>SVELTEKIT-REST</p>
-
-<button
-	on:click={async () => {
-		console.log(await client.users({ name: 'Kirthevasen' }));
-	}}>Click me</button
->
